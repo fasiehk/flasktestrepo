ed75d84ac00a42780329ec66440ec3cc195f225d
fasiehk <fasiehklasson@gmail.com>
2025-04-20 16:21:24 -0400

    Fix for Railway deployment

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ app = create_app()  # Use the refactored create_app function
 with app.app_context():
     app.register_blueprint(shortener_bp)
 
-# Run the app locally
+# Run the app locally or on Railway
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=5000)  # Explicitly set host and port
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment variables
+    app.run(debug=False, host="0.0.0.0", port=port)  # Bind to 0.0.0.0 for external access
